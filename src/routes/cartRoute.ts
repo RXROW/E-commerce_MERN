9fed33254200f4c120e5fb6860b16878764daa08
“Eslam“ <aslamalmswdya@gmail.com>
2024-07-14 22:10:31 +0300

    Update Cart Item enpoint / PUT

--- src/routes/cartRoute.ts
+++ src/routes/cartRoute.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { addItemToCart, getAcitveCartForUser } from "../services/cartService";
+import { addItemToCart, getAcitveCartForUser, updateItemInCart } from "../services/cartService";
 import validateJWT from "../middelwares/validateJWT";
 
 declare module "express-serve-static-core" {
@@ -20,6 +20,16 @@ router.post("/items", validateJWT, async (req, res) => {
   const userId = req.user._id;
   const {productId ,quantity } = req.body;
   const response = await addItemToCart({ userId ,productId ,quantity });
+  res.status(response.statusCode).send(response.data);
+});
+
+
+
+
+router.put("/items", validateJWT, async (req, res) => {
+  const userId = req.user._id;
+  const {productId ,quantity } = req.body;
+  const response = await updateItemInCart({ userId ,productId ,quantity });
   res.status(response.statusCode).send(response.data);
 });
 
--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -58,8 +58,8 @@ export const addItemToCart = async ({
   }
 
   // handel Stock
-  if(product.stock < quantity){
-    return {data:"Low Stock For Item ! " ,statusCode: 400 }
+  if (product.stock < quantity) {
+    return { data: "Low Stock For Item ! ", statusCode: 400 };
   }
 
   const newItem: ICartItem = {
@@ -68,12 +68,65 @@ export const addItemToCart = async ({
     quantity,
   };
   cart.items.push(newItem);
-  // Update TotalAmount 
+  // Update TotalAmount
   cart.totalAmount += product.price * quantity;
 
   const updatedCart = await cart.save();
 
-  console.log("Updated Cart:", updatedCart);
+  return { data: updatedCart, statusCode: 201 };
+};
+
+// Update Item In Cart
+interface UpdateItemInCart {
+  productId: string;
+  quantity: number;
+  userId: string;
+}
+
+export const updateItemInCart = async ({
+  userId,
+  productId,
+  quantity,
+}: UpdateItemInCart) => {
+  if (!mongoose.Types.ObjectId.isValid(productId)) {
+    return { data: "Invalid Product ID", statusCode: 400 };
+  }
+
+  const cart = await getAcitveCartForUser({ userId });
+  if (!cart) {
+    return { data: "Cart Not Found!", statusCode: 404 };
+  }
+
+  const existInCart = cart.items.find(
+    (p) => p.product.toString() === productId.toString()
+  );
+  if (!existInCart) {
+    return { data: "Item does not  exists in Cart!", statusCode: 404 };
+  }
+
+  const product = (await productModel.findById(productId)) as IProduct;
+  if (!product) {
+    return { data: "Product Not Found!", statusCode: 404 };
+  }
+  // handel Stock
+  if (product.stock < quantity) {
+    return { data: "Low Stock For Item ! ", statusCode: 400 };
+  }
+
+  const otheCartItems = cart.items.filter((p) => {
+    p.product.toString() !== productId;
+  });
+
+  console.log(otheCartItems);
+  let total = otheCartItems.reduce((sum, product) => {
+    sum += product.quantity * product.unitPrice;
+    return sum;
+  }, 0);
+  
+  existInCart.quantity = quantity;
+  total = existInCart.quantity * existInCart.unitPrice;
+  cart.totalAmount = total;
+  const updatedCart = await cart.save();
 
   return { data: updatedCart, statusCode: 201 };
 };
